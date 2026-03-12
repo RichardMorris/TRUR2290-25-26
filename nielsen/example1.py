7af3a60f17441747439f43001093410977e45169
Richard Morris <rich@singsurf.org>
2026-03-12 19:24:11 +0000

    better output info

--- nielsen/example1.py
+++ nielsen/example1.py
@@ -26,15 +26,18 @@ def main():
     trd = list(full_td) 
     tsd = list(testdata)
 
-    layers = [784, 32, 16, 16, 10]
+    layers = [784, 16, 16, 10]
     epochs = 10 # Number of epochs to train for
     batch_size = 10
     eta = 3.0
 
     print("\n{0} hidden layers: {1}".format(len(layers)-2,layers))
+    print("epochs",epochs)
+    print("batch size", batch_size)
+    print("eta",eta)
     net = network.Network(layers)
     weights = [int(np.prod(np.shape(x))) for x in net.weights]
-    print("weights",weights," sum ",sum(weights))
+    print("weights",["{0}x{1}".format(np.shape(x)[0],np.shape(x)[1]) for x in net.weights]," sum ",sum(weights))
     bias = [len(x) for x in net.biases]
     print("biases", bias," sum ",sum(bias))
     print("total parameters ", sum(weights) + sum(bias))
